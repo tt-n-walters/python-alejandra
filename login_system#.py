7054616b52bfb569abbf69f37d281024f589a772
ttnw <nico.walters@techtalents.es>
2022-03-24 14:00:25 +0100

    Added login system start point

---
+++ login_system#.py
@@ -0,0 +1,6 @@
+usernames = ["arthur", "bill", "charlie", "dobby", "errol", "fred", "george", "harry"]
+passwords = ["weasley123", "curses456", "dragons789", "harry123", "treats456", "george789", "fred123", "snitch456"]
+
+user_input = input("Enter username: ")
+password_input = input("Enter password: ")
+
